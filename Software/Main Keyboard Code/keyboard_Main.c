4066f10a5e164ad3df3bddc97ed6299f135edeb4
Artgameur <charlesantoinemailhot@gmail.com>
2026-06-13 16:29:18 -0400

    Add files via upload

--- Software/Main Keyboard Code/keyboard_Main.c
+++ Software/Main Keyboard Code/keyboard_Main.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026  Charles-Antoine Mailhot
+ * Copyright (C) 2026  Artgameur
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
